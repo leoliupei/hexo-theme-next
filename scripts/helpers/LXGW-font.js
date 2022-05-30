d7316e525a07622a95d44e233a1a48e976c59d3f
liupei <liupei.me@gmail.com>
2022-05-31 02:09:27 +0800

    改字体CDN

--- scripts/helpers/LXGW-font.js
+++ scripts/helpers/LXGW-font.js
@@ -2,5 +2,5 @@
 
 // https://developers.google.com/fonts/docs/getting_started
 module.exports = function() {
-  return `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/lxgw-wenkai-webfont@1.1.0/style.css" />`;
+  return `<link rel="stylesheet" href="https://unpkg.com/lxgw-wenkai-webfont@1.1.0/style.css" />`;
 };
